79b58b6014cec236d9e006abc0141c5590e40266
keristo <casetiadharma@gmail.com>
2022-11-30 13:17:43 +0800

    fix features number

--- generate_feature.py
+++ generate_feature.py
@@ -3,10 +3,12 @@ from a ndarray structure e.g. (119, 74, 8,7) or (20,20, 8,7), generate feature o
 for e.g. a feature of the tree can be any combination of MEAN, VAR, SKEW, KURTOSIS of
 ANY CHANNELS of ANY FEATURES
 Example:
-np.mean[:,:, i, j] where i=7, j=8 means MEAN_GREEN_CORRELATION of the tree
+np.mean[:,:, i, j] where i=7, j=6 means MEAN_GREEN_CORRELATION of the tree
 '''
 
 '''
+CHANNELS : 
+
 Wideband Red = 0
 Wideband Green = 1
 Wideband Blue = 2
@@ -17,33 +19,33 @@ Red = 6
 Green = 7
 '''
 
-#features:
+
 '''
+FEATURES : 
+
 NONE = 0
 HOMOGENEITY = 1
 CONTRAST = 2
 ASM = 3
-MEAN_I = 4
-MEAN_J = 5
-
-VAR_I = 6
-VAR_J = 7
-CORRELATION = 8
+MEAN = 4
+VAR = 5
+CORRELATION = 6
 '''
+
 import numpy as np
 from glcm_loader import load_glcm
 
 def generate_feature_from_tree(tree):
     tree_features = []
-    for i in range(tree.shape[-2]):
-        for j in range(tree.shape[-1]):
+    for i in range(tree.shape[-2]): # for i in channels
+        for j in range(tree.shape[-1]): # for j in features
             tree_features.append(np.mean(tree[:,:, i, j].flatten())) # is flatten the right way to do it?
     #tree_features_series = pd.Series(tree_features) # make it so easier to put in table later
     return tree_features
 
-# #%%
-# tree = load_glcm('data/glcm_18Dec2020_3rad_2step_128bins_1xDownScale_Clausena Excavata_11.npz')
-# feature = generate_feature_from_tree(tree)
+#%%
+tree = load_glcm('data/dec/glcm_18Dec2020_3rad_2step_128bins_1xDownScale_Clausena Excavata_11.npz')
+feature = generate_feature_from_tree(tree)
 #
 # # feature[0] would be MEAN_WBRED_NONE
 # #feature[1] would be MEAN_WBRED_HOMOGEINITY
